f2f68d64b635a618065fde184195a245354de283
bagasm24 <mbagas368@gamil.com>
2023-11-12 14:39:08 +0700

    menambahkan seeder user

---
+++ seeders/20231112073138-user.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        firstName: "Alex",
+        lastName: "Doe",
+        email: "alexdoe@gmail.com",
+        password: "123",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
